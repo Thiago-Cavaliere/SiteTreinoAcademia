b25a564fa82c0ae48fc49eefa41595f5ae42d798
thiagocavaliere1234@gmail.com <thiagocavaliere1234@gmail.com>
2025-06-16 04:41:08 -0300

    pag home

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,11 @@
+import style from "./page.module.css";
+
 export default function Page() {
   return (
-    <>
-      teste
-    </>
+    <main className={style.fundo}>
+      <h2 className={style.page}>
+        Seja bem vindo ao site de treinos de academia!
+      </h2>
+    </main>
   );
 }
